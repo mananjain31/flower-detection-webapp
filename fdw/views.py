5d43c3c3b1f840c596605aef4086157028777bb7
Jash <jashsharma619@gmail.com>
2022-10-14 13:43:22 +0530

    Base64 Under development

--- fdw/views.py
+++ fdw/views.py
@@ -19,11 +19,13 @@ def test(request):
 def imgPost(request):
     if request.method == 'POST':
         jsonData = json.loads(request.body)
-        ImgFinal = ImgBin2Numpy(jsonData['Image'])
-        print(ImgFinal)
+        # ImgFinal = ImgBin2Numpy(jsonData['Image'])
+        # print(ImgFinal)
+        base64String = jsonData['Image']
+        NumpyImage = ImgBin2Numpy(base64String)
         return JsonResponse({ 
           "success":True,
-
+          "Image":str(NumpyImage)
          })
 
 
