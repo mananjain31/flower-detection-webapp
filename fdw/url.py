86a409256bff5f7de427cf620006d8b6a4357d5b
Aditya Pathak <sumitpathak861@gmail.com>
2022-10-14 12:45:59 +0530

    imgPost Views And Url Created

--- fdw/url.py
+++ fdw/url.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('test/', views.test),
+    path('imgPost/',views.imgPost),
+
 ]
--- fdw/views.py
+++ fdw/views.py
@@ -1,7 +1,20 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+import json
+import base64
 
 # Create your views here.
 def test(request):
     if request.method == 'GET':
         print('This is a Test')
+
+def imgPost(request):
+    if request.method == 'POST':
+        jsonData = json.loads(request.body)
+        return JsonResponse({ 
+          "success":True,
+          
+         })
+        
+
     
